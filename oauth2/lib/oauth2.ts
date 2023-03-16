b43ea001508e6d07e5a0689e54add56a6d5306de
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-03-17 02:45:34 +0530

    oAuth2Client init

--- oauth2/lib/oauth2.ts
+++ oauth2/lib/oauth2.ts
@@ -1,1 +1,10 @@
-export class OAuth2Instance {}
+import { OAuth2App } from "../OAuth2App";
+export class OAuth2Client {
+    constructor(private readonly app: OAuth2App) {}
+    public async login() {
+        return;
+    }
+    public async logout() {
+        return;
+    }
+}
